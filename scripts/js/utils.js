3274905acdf43c253726d7855620a40d8ad23462
Marcin Michałek <marcin.marcim@gmail.com>
2015-01-04 11:32:01 +0100

    use jquery, create table with ranking based on rounds

--- scripts/js/utils.js
+++ scripts/js/utils.js
@@ -2,9 +2,26 @@
  * Created by Marcin on 2015-01-03.
  */
 var source = new EventSource("http://localhost:8080/alertTest");
-source.addEventListener('changed', function(e) {
-    var p = document.createElement("p");
-    p.innerHTML = e.data;
-    document.getElementById("listOfDrinkers").appendChild(p);
-    console.log(e.data);
+source.addEventListener('changed', function (event) {
+    createTableFromDrinkersList(JSON.parse(event.data))
+    console.log(event.data);
 }, false);
+
+function createTableFromDrinkersList($drinkersList) {
+    clearDrinkersList()
+
+    var table = $(document.createElement('table')).addClass('table-fill');
+    for (var i = 0; i < $drinkersList.length; i++) {
+        var tdName = $('<td></td>').addClass('text-left').text($drinkersList[i]["name"]);
+        var tdRounds = $('<td></td>').addClass('text-left').text($drinkersList[i]["drinkingParameters"]["rounds"] + " rounds");
+        var row = $(document.createElement('tr'));
+        row.append(tdName);
+        row.append(tdRounds);
+        table.append(row);
+    }
+    $('#listOfDrinkers').append(table);
+}
+
+function clearDrinkersList() {
+    document.getElementById("listOfDrinkers").innerHTML = "";
+}
